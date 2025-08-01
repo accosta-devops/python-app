6e5a72780f87b9a0f576d227e4e4a546e6ab53f1
Anderson Carvalho da Costa <andersonccosta.devops@gmail.com>
2025-07-31 21:21:28 -0300

    init commit

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import datetime, socket
+
+app = Flask(__name__)
+
+@app.route("/api/v1/details")
+
+def details():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%d/%m/%Y"),
+        'hostname': socket.gethostname() 
+    })
+
+@app.route("/api/v1/healthz")
+
+def health():
+    return jsonify({
+        'status': 'up'
+    }), 200
+
+
+if __name__=='__main__':
+
+    app.run()
+
